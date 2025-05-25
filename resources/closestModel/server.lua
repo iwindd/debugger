c50758f183dc78e8bf53bb1bfab6d732a6329c4f
iwindd <freewinendlife@gmail.com>
2025-05-25 16:50:39 +0700

    Add distance parameter to getClosestModelData command for enhanced object search

--- resources/closestModel/server.lua
+++ resources/closestModel/server.lua
@@ -1,7 +1,8 @@
 lib.addCommand("getClosestModelData", {
   restricted = "group.admin",
   params = {
-    {name = "modelFilterName", type = "string", optional = true, help = "The model name to search for. If not provided, it will use the closest object model."}
+    {name = "modelFilterName", type = "string", optional = true, help = "The model name to search for. If not provided, it will use the closest object model."},
+    {name = "distance", type = "number", optional = true, help = "The distance to search for objects, default is 10."}
   }
 }, function (source, args, raw)
   TriggerClientEvent("debugger.closestModel:get", source, args.modelFilterName)
